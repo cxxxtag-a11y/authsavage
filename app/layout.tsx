ed7333b43243d01c6908cce216d0344098ab6639
cxxxtag-a11y <cxxxtag@gmail.com>
2026-03-15 22:12:42 -0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Discord OAuth",
+  description: "Login com Discord",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
